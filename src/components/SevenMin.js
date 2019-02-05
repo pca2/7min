0fb92ceba42d7ebfc87bfa64c3511393ee33a18f
Carleton <atwaterc@gmail.com>
2019-02-05 12:21:44 -0500

    Ardd basic Timer functionality
    Still needs: Reset functionality,  Pause vs Stop, etc

--- src/components/SevenMin.js
+++ src/components/SevenMin.js
@@ -1,13 +1,69 @@
 import React from 'react';
 import Exercise from './Exercise';
 import Timer from './Timer';
+const exerciseList = [
+  'Center Shoulders',
+  'Front Shoulders',
+  'Back Shoulders',
+  'Right Suitcase',
+  'Left Suitcase',
+  'Swing',
+  'Pushups',
+  'Sit-ups'
+];
+const timerLength = 30;
 
-const SevenMin = () => (
-  <div className="SevenMin">
-    <Exercise name="Sit-ups" />
-    <Timer time={30} />
-  </div>
-);
+
+class SevenMin extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      time: timerLength,
+      activity: exerciseList[0],
+      timerRunning: false
+    }
+    this.toggleTimer = this.toggleTimer.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.tick = this.tick.bind(this);
+  };
+  
+  toggleTimer(){
+    this.setState(prevState => ({
+      timerRunning: !prevState.timerRunning
+    }));
+  }
+  
+  startTimer(){
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+    this.toggleTimer();
+  };
+  
+  stopTimer(){
+    clearInterval(this.timerID);
+    this.toggleTimer();
+  }
+  
+  tick(){
+    if(this.state.time <= 1){
+      this.stopTimer();
+    };
+    this.setState((prevState) => ({
+      time: prevState.time - 1
+    }))
+  }
+  
+  render(){
+    return(
+      <div className="SevenMin">
+        <Exercise name={this.state.activity} />
+        <Timer time={this.state.time} startTimer={this.startTimer} stopTimer={this.stopTimer}  />
+      </div>
+    )
+  }
+};
 
 
 export default SevenMin;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Timer = props => {
-  const {time} = props;
+  const {time, startTimer, stopTimer} = props;
   return(
     <div className="Timer">
       {time}
       <div className="controls">
-        <button id="play" name="play">Play</button>
-        <button id="stop" name="stop">Stop</button>
+        <button id="play" name="play" onClick={startTimer} >Play</button>
+        <button id="stop" name="stop" onClick={stopTimer} >Stop</button>
       </div>
     </div>
   )
