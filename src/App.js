e9b542ae9254338372188fa5de969bd729fd2615
pca2 <atwaterc@gmail.com>
2019-02-04 21:00:16 -0500

    Add sevenmin component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import SevenMin from './components/SevenMin';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SevenMin/>
       </div>
     );
   }
