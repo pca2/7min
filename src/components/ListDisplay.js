29b877fced64f61c3b382e4a52acb00cfd3d12a6
Carleton <atwaterc@gmail.com>
2019-02-08 14:04:32 -0500

    Fix my dumb ListDisplay code, and add basic css for it
    It needs to be outside of SevenMin though. Figure that out

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -6,8 +6,10 @@ const ListDisplay = props => {
   return(
     <div className="ListDisplay">
       <ul>
-        {props.List}.map(item => <ListItem item={item}/>)
+        {props.list.map((item,index) => <ListItem key={index} item={item}/>)}
        </ul>
      </div>
   )
 }
+
+export default ListDisplay;
